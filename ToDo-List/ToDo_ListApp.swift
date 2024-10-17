6a3ef3292659e5705df4fb31389800e4fa2d64b4
Garbe, Lennart (SE-A/35) <lennart.garbe@volkswagen.de>
2024-10-17 09:18:08 +0200

    Initial Commit

---
+++ ToDo-List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo-List
+//
+//  Created by Garbe, Lennart (SE-A/32) on 17.10.24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDo-ListTests/ToDo_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListTests.swift
+//  ToDo-ListTests
+//
+//  Created by Garbe, Lennart (SE-A/32) on 17.10.24.
+//
+
+import Testing
+@testable import ToDo_List
+
+struct ToDo_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
